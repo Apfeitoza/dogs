c9781942a8e2f403f364738dbe9c7ca2b14d6842
Apfeitoza <annapaulaasf@gmail.com>
2026-06-10 16:35:37 -0300

    feat: insere seção de login com fetch da api cria useForm para lidar com formularios cria componentes de input e botões cria funções para otimizar o fetch da api cria contexto de usuario"

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Home from './components/Home'
-import Login from './components/login/Login'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Login from './components/login/Login';
+import { UserStorage } from './UserContext';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-       <Header />
-       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/' element={<Login/>} />
-       </Routes>
-      <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
-     
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
